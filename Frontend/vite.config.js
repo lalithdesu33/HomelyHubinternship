7d4e408a50f15436b26c4f0c194d07a261bb08bb
lalithdesu33 <lalithdesu33@gmail.com>
2026-09-19 10:59:54 +0530

    Add deployed backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homelyhubinternship-9w4x.onrender.com',
         changeOrigin: true,
         secure: false,
       }
